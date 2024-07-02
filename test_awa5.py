8174e17ecac3fe3b9a151b2f6cc027e3045f07e5
Jutastre <pukarlindgren@gmail.com>
2024-07-02 02:24:46 +0200

    more better tests ir_to_awa.py file made

--- test_awa5.py
+++ test_awa5.py
@@ -12,8 +12,8 @@ PASS_STRING = f"{ANSI_GREEN}PASS{ANSI_WHITE}"
 FAIL_STRING = f"{ANSI_RED}FAIL{ANSI_WHITE}"
 
 
-output_buffer: str = ""
-input_buffer: str = ""
+output_buffer: str
+input_buffer: str
 
 
 def test_output_function(string: str):
@@ -25,22 +25,39 @@ def test_input_function():
     global input_buffer
     return input_buffer
 
+def load_tests(filename:str) -> list:
+    with open(filename) as file:
+        testcases = [
+            (
+                test_label.strip("\n"),
+                code.strip("\n"),
+                input.strip("\n"),
+                expected_output.strip("\n"),
+            )
+            for test_label, code, input, expected_output in itertools.batched(
+                file.read().split("!")[:-1], 4
+            )
+        ]
+    return testcases
 
-with open("test_cases.txt") as file:
-    testcases = [
-        (code.rstrip('\n'), input.rstrip('\n'), expected_output.rstrip('\n'))
-        for code, input, expected_output in itertools.batched(file.readlines(), 3)
-    ]
+def run_tests(testcases:list) -> None:
+    global input_buffer, output_buffer
 
-for test_idx, (code, input, expected_output) in enumerate(testcases):
-    output_buffer = ""
-    input_buffer = input
-    
-    vm = AwaVM(output_function=test_output_function)
-    vm.run_program(code)
+    for test_idx, (test_label, code, input, expected_output) in enumerate(testcases):
+        output_buffer = ""
+        input_buffer = input
 
-    passed = output_buffer == expected_output
-    print(f"Test #{test_idx + 1}: {PASS_STRING if passed else FAIL_STRING}")
-    if not passed:
-        print(f"Expected: {expected_output}")
-        print(f"Got: {output_buffer}")
+        vm = AwaVM(output_function=test_output_function, input_function=test_input_function)
+        vm.run_program(code)
+
+        passed = output_buffer.strip("\n") == expected_output
+        print(f"Test #{test_idx + 1}: {PASS_STRING if passed else FAIL_STRING}")
+        if not passed:
+            print(f"Expected: {expected_output}")
+            print(f"Got: {output_buffer}")
+
+if __name__ == '__main__':
+
+    tests = load_tests("test_cases.txt")
+
+    run_tests(tests)
---
+++ tools/ir_to_awa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+import re #oops i did it again
+
